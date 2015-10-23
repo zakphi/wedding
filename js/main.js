754b16df9b6691339798e3b8f0afc8293a4eb293
Philip Zak <philipkzak@gmail.com>
2015-10-23 15:51:50 -0400

    fixed grunt issue

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 $(function(){
   $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
       var $target = $(this.hash);
       $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
       if ($target.length) {
